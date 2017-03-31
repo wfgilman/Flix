d7ad7b218313f0cf0323c1755f7d246e1da8e404
Will Gilman <will@samadhi.tech>
2017-03-31 13:57:49 -0700

    load low-res then high-res image

--- Flix/DetailVC.swift
+++ Flix/DetailVC.swift
@@ -31,10 +31,35 @@ class DetailVC: UIViewController {
         
         overviewLabel.sizeToFit()
         
-        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let baseUrl = "https://image.tmdb.org/t/p"
+        let smallSize = "/w500"
+        let largeSize = "/original"
         if let posterPath = movie["poster_path"] as? String {
-            let posterUrl = NSURL(string: baseUrl + posterPath)
-            posterImageView.setImageWith(posterUrl as! URL)
+            let smallposterRequest = NSURLRequest(url: NSURL(string: baseUrl + smallSize + posterPath) as! URL)
+            let largeposterRequest = NSURLRequest(url: NSURL(string: baseUrl + largeSize + posterPath) as! URL)
+            posterImageView.setImageWith(
+                smallposterRequest as URLRequest,
+                placeholderImage: nil,
+                success: { (smallposterRequest, smallPosterResponse, smallPoster) -> Void in
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = smallPoster
+                    UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                        self.posterImageView.alpha = 1.0
+                    }, completion: { (success) -> Void in
+                        self.posterImageView.setImageWith(
+                            largeposterRequest as URLRequest,
+                            placeholderImage: smallPoster,
+                            success: { (largePosterRequest, largePosterResponse, largePoster) -> Void in
+                                self.posterImageView.image = largePoster
+                            },
+                            failure: { (request, response, error) -> Void in
+                                print(error)
+                            })
+                    })
+                },
+                failure: { (request, response, error) -> Void in
+                    print(error)
+                })
         }
     }
 
--- README.md
+++ README.md
@@ -20,7 +20,7 @@ The following **optional** features are implemented:
 - [ ] Implement segmented control to switch between list view and grid view.
 - [x] Add a search bar.
 - [x] All images fade in.
-- [ ] For the large poster, load the low-res image first, switch to high-res when complete.
+- [x] For the large poster, load the low-res image first, switch to high-res when complete.
 - [ ] Customize the highlight and selection effect of the cell.
 - [ ] Customize the navigation bar.
 
